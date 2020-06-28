b632b94e8ef78a3cebb5c0843c722c5178448b00
Mohit Sharma <mohit7153@gmail.com>
2020-06-28 22:26:16 +0530

    Prject 4 completed

---
+++ ExchangeRateCalculator/script.js
@@ -0,0 +1,36 @@
+const currency_one = document.getElementById('currency-one');
+const amount_one = document.getElementById('amount-one');
+const currency_two = document.getElementById('currency-two');
+const amount_two = document.getElementById('amount-two');
+
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate(){
+    
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one.value}`)
+        .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                const value = data.rates[currency_two.value];
+                rate.innerText = `1 ${currency_one.value} = ${value} ${currency_two.value}`;
+
+                amount_two.value = amount_one.value * value.toFixed(2);
+            });
+
+}
+
+currency_one.addEventListener('change',calculate);
+currency_two.addEventListener('change',calculate);
+amount_one.addEventListener('input',calculate);
+amount_two.addEventListener('input',calculate);
+
+swap.addEventListener('click',function(){
+    const temp = currency_one.value;
+    currency_one.value = currency_two.value;
+    currency_two.value = temp;
+    calculate();
+});
+
+calculate();
